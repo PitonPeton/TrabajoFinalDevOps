8f9f6e46a519b55d6d1c4e8288218586a8478ec0
PitonPeton <magnothrilex@gmail.com>
2024-08-11 21:46:18 -0400

    arreglado 3

--- script.js
+++ script.js
@@ -3,5 +3,4 @@ const imprimirMensaje = () => {
   return "¡Hola Mundo!"
 }
 
-// Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = imprimirMensaje;
--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,6 @@
-// tests/script.test.js
 const imprimirMensaje = require('./script');
 
 test('El script imprime "Hola Mundo"', () => {
-    console.log = jest.fn();  // Mock de console.log
-    imprimirMensaje();
-    expect(console.log).toHaveBeenCalledWith("Hola Mundo");
-});
-
-test('debería imprimir "¡Hola Mundo!" en la consola', ()=> {
-  expect(imprimirMensaje()).toBe("¡Hola Mundo!");
+    console.log = jest.fn();
+    expect(imprimirMensaje()).toBe("¡Hola Mundo!");
 });
